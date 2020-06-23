db78c4ae5f35af4e2ab386e2bc4759b8a4c4be88
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-23 02:52:06 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person 
+ def name=(person_name)
+  @name=person_name 
+ end 
+ def name 
+  @name 
+ end  
+ def job=(person_job)
+ @job=person_job 
+ end 
+ def job 
+ @job 
+ end 
+end 
+
+
+
+#def name=(dog_name)
+#   @name= dog_name
+# end
+# 
+# def name
+#   @name
+# end
